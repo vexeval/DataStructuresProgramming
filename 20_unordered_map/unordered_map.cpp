8e361376e4b6209591bea863b3900681aab5a134
Matt <mshook7298@floridapoly.edu>
2026-04-20 20:50:05 +0000

    hashtable fix, unordered maps

---
+++ 20_unordered_map/unordered_map.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <unordered_map> // hash table
+#include <vector>
+#include <string>
+
+class HashExamples {
+public:
+    template <typename T>
+    static void countFrequency(const std::vector<T>& v) {
+        std::unordered_map<T, int> freq;
+        for (const T& i : v) {
+            freq[i]++;
+        }
+        for (const auto& f : freq)
+        {
+            std::cout << f.first << " : " << f.second << std::endl;
+        }
+
+    }
+
+    template <typename T>
+    static bool hasDuplicates(const std::vector<T> v) {
+        std::unordered_map<T, int> vals;
+        for (const T& i : v) {
+            vals[i]++;
+        }
+        
+        for (const auto& i : vals) {
+            if (i.second > 1) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    static char firstNonRepeatingChar(const std::string& s);
+    
+    static void groupStrings(const std::vector<std::string>& strings);
+
+    private:
+
+};
+
+int main(void)
+{
+    std::unordered_map<std::string, int> grades;
+
+    // Insert
+    grades["Mitch"] = 90;
+    grades["Jon"] = 80;
+    grades["Andrew"] = 93;
+
+    // Iterate through the table
+    for (const auto& grade : grades)
+    {
+        std::cout << grade.first << " : " << grade.second << std::endl;
+    }
+
+    std::cout << std::endl;
+    
+    // Search
+    auto it = grades.find("Mitch");
+    if (it == grades.end()) { // didn't find--no such key in table
+        std::cout << "Not found\n";
+    }
+    else {
+        std::cout << "Found: \n";
+        std::cout << it->first << " : " << it->second << std::endl;
+    }
+
+    std::cout << "Frequency counts in v:\n";
+    std::vector<int> v {1, 2, 3, 4, 1, 2, 2, 2, 2, 5, 5};
+    HashExamples::countFrequency(v);
+    
+    std::cout << "Check duplicates:\n";
+    std::vector<std::string> d {"hello", "apple", "banana", "hey", "ban", "apple", "orange"};
+    std::cout << HashExamples::hasDuplicates(d) << std::endl;
+
+    return -34.3;
+}
